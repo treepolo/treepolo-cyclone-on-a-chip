44b0b0df9d97e7089d153d552a2102d899603227
treepolo <bigtreepolo@gmail.com>
2026-09-04 01:30:15 +0800

    stage4: apply model-top sponge in CPU rotating core

--- src/solver/rotatingDryCoreCpu.ts
+++ src/solver/rotatingDryCoreCpu.ts
@@ -3,16 +3,26 @@ import { Vec3, dot3, scale3, sub3 } from '../core/math.js';
 import { CubedSphereGrid } from '../grid/cubedSphere.js';
 import { VerticalGrid } from '../grid/vertical.js';
 import { applyHeldSuarezForcing } from '../physics/heldSuarez.js';
+import { applyModelTopSponge } from '../physics/modelTopSponge.js';
 import { ReferenceAtmosphere } from '../physics/referenceAtmosphere.js';
 import { RotationGeometry, addCellWindDeltaToEdges, applyTraditionalCoriolis, buildRotationGeometry, reconstructCellHorizontalWind } from '../physics/rotation.js';
 import { DryCoreCpu, StepDiagnostics, TransportSnapshot } from './dryCoreCpu.js';
 import { DryState, cell3DIndex, edge3DIndex, w3DIndex } from './state.js';
 
-export interface RotatingStepOptions{heldSuarez?:boolean;momentumTransport?:boolean;coriolis?:boolean}
+export interface RotatingStepOptions{heldSuarez?:boolean;momentumTransport?:boolean;coriolis?:boolean;topSponge?:boolean}
 export class RotatingDryCoreCpu{
   readonly dry:DryCoreCpu;readonly rotation:RotationGeometry;
   constructor(public readonly h:CubedSphereGrid,public readonly v:VerticalGrid,public readonly ref:ReferenceAtmosphere){this.dry=new DryCoreCpu(h,v,ref);this.dry.captureTransport=true;this.rotation=buildRotationGeometry(h)}
-  step(s:DryState,dt:number,opt:RotatingStepOptions={}):StepDiagnostics{const cor=opt.coriolis!==false,mom=opt.momentumTransport!==false,hs=opt.heldSuarez!==false;if(cor)applyTraditionalCoriolis(this.h,this.rotation,s,.5*dt);const d=this.dry.step(s,dt);if(mom){if(!this.dry.lastTransport)throw new Error('missing transport snapshot');this.advectMomentum(s,dt,this.dry.lastTransport)}if(hs)applyHeldSuarezForcing(this.h,this.v,s,dt);if(cor)applyTraditionalCoriolis(this.h,this.rotation,s,.5*dt);return d}
+  step(s:DryState,dt:number,opt:RotatingStepOptions={}):StepDiagnostics{
+    const cor=opt.coriolis!==false,mom=opt.momentumTransport!==false,hs=opt.heldSuarez!==false,sponge=opt.topSponge!==false;
+    if(cor)applyTraditionalCoriolis(this.h,this.rotation,s,.5*dt);
+    const d=this.dry.step(s,dt);
+    if(mom){if(!this.dry.lastTransport)throw new Error('missing transport snapshot');this.advectMomentum(s,dt,this.dry.lastTransport)}
+    if(hs)applyHeldSuarezForcing(this.h,this.v,s,dt);
+    if(sponge)applyModelTopSponge(this.v,s,dt);
+    if(cor)applyTraditionalCoriolis(this.h,this.rotation,s,.5*dt);
+    return d;
+  }
   private advectMomentum(s:DryState,dt:number,t:TransportSnapshot):void{
     const{h,v}=this,nz=v.nz,R=EARTH.radius,windByK:Float64Array[]=Array.from({length:nz},(_,k)=>reconstructCellHorizontalWind(h,this.rotation,s,k)),hmx=new Float64Array(s.uEdge.length),hmy=new Float64Array(s.uEdge.length),hmz=new Float64Array(s.uEdge.length);
     for(let e=0;e<h.edgeCount;e++)for(let k=0;k<nz;k++){const q=edge3DIndex(e,k,nz),m=t.hMassFlux[q]!,src=m>=0?h.edges[e]!.leftCell:h.edges[e]!.rightCell,w=windByK[k]!;hmx[q]=m*w[src*3]!;hmy[q]=m*w[src*3+1]!;hmz[q]=m*w[src*3+2]!}
